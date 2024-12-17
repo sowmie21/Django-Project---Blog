9a750c31455ba45baa89f893cdb1b9b6507004d5
Sowmya <sssk062003@gmail.com>
2024-12-17 13:28:12 +0530

    Add .gitignore to exclude venv folder

---
+++ myApp/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+def detail(request, post_id):
+    return render(request,'detail.html')
+
+
+
+def old_url_redirect(request):
+    return redirect(reverse('blog:new_url'))
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
